e00f4cba13be948b2c3a265f12a62f5c3b336379
Mark Wilson <mwilson@rippleffect.com>
2016-02-11 13:55:36 +0000

    Remove deprecated Twig autoloader

--- build.php
+++ build.php
@@ -2,8 +2,6 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-Twig_Autoloader::register();
-
 $loader = new Twig_Loader_Filesystem([__DIR__ . '/pages', __DIR__ . '/layouts']);
 $twig = new Twig_Environment($loader, [
     'cache' => __DIR__ . '/cache'
