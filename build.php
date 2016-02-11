b6bbf0c09636219e41306d35986d2b07b149ea78
Mark Wilson <mwilson@rippleffect.com>
2016-02-11 13:48:01 +0000

    Use twig to compile index and 404 pages

---
+++ build.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem([__DIR__ . '/pages', __DIR__ . '/layouts']);
+$twig = new Twig_Environment($loader, [
+    'cache' => __DIR__ . '/cache'
+]);
+
+$pages = glob(__DIR__ . '/pages/*.html.twig');
+
+foreach ($pages as $page) {
+    $templateFile = substr($page, strlen(__DIR__ . '/pages/'));
+    $pathInfo = pathinfo($templateFile);
+
+    $template = $twig->loadTemplate($templateFile);
+    file_put_contents(__DIR__ . '/public/' . $pathInfo['filename'], $template->render([]));
+}
+
+`rsync -a static public/assets`;
