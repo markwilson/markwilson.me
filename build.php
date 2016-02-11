85c15ec1b207ad0d5a4f71c3478a1ee20fa0f6e8
Mark Wilson <mwilson@rippleffect.com>
2016-02-11 13:55:42 +0000

    Add TODO comments

--- build.php
+++ build.php
@@ -14,7 +14,13 @@ foreach ($pages as $page) {
     $pathInfo = pathinfo($templateFile);
 
     $template = $twig->loadTemplate($templateFile);
+
+    // TODO: pass variables through to the template engine
     file_put_contents(__DIR__ . '/public/' . $pathInfo['filename'], $template->render([]));
 }
 
+// TODO: move this to a better place
 `rsync -a static/ public/assets/`;
+
+// TODO: test with sub-directories
+// TODO: upload lambda function to AWS automatically on each deploy
