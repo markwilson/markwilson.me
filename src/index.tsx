0e649a25a8cafc4a4a013af016e705136a544485
Mark Wilson <markwilson@users.noreply.github.com>
2020-07-20 21:13:01 +0100

    Check font is loaded before loading app

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,36 @@ import "./loader.css";
 const root = document.getElementById("root") as HTMLDivElement;
 setTimeout(() => {
   if (root.childElementCount === 0) {
-    root.innerHTML = "<p id=\"loader\">Loading &hellip;</p>";
+    root.innerHTML = '<p id="loader">Loading &hellip;</p>';
   }
 }, 1500);
 
+// document.fonts isn't fully supported yet
+declare global {
+  interface Document {
+    fonts: FontFaceSet;
+  }
+}
+
+declare global {
+  interface FontFaceSet {
+    load: (arg0:string) => Promise<any>;
+  }
+}
+
 import(/* webpackChunkName: "bootstrap" */ "./bootstrap").then((module) => {
   const bootstrap = module.default;
-  bootstrap();
+
+  if (document.fonts && document.fonts.load) {
+    Promise.all([
+      document.fonts.load("400 2rem Roboto"),
+      document.fonts.load("300 1.6rem Roboto"),
+    ]).then(() => {
+      bootstrap();
+    });
+  } else {
+    bootstrap();
+  }
 });
 
 export default null;
