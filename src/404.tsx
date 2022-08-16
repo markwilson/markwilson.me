b92a1cb6680a29101aed1b8381522ff150d66f92
Mark Wilson <markwilson@users.noreply.github.com>
2022-08-16 19:41:06 +0100

    Remove React

--- src/404.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client";
-import PageNotFound from "./PageNotFound";
-
-const container = document.getElementById("root");
-const root = createRoot(container!);
-root.render(<PageNotFound />);
