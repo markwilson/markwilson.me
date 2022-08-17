1230326b9af636206f58f4b09d9264b180a18c47
Mark Wilson <markwilson@users.noreply.github.com>
2022-08-17 16:42:01 +0100

    Add AAAA DNS record

--- main.tf
+++ main.tf
@@ -2,11 +2,19 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_route53_record" "site" {
+resource "aws_route53_record" "a_record" {
   zone_id = "Z2NO2HNDAEBJY7"
   name    = "markwilson.me"
   type    = "A"
   records = ["185.199.108.153", "185.199.109.153", "185.199.110.153", "185.199.111.153"] # GitHub IPs
+  ttl     = 3600
+}
+
+resource "aws_route53_record" "aaaa_record" {
+  zone_id = "Z2NO2HNDAEBJY7"
+  name    = "markwilson.me"
+  type    = "A"
+  records = ["2606:50c0:8000::153", "2606:50c0:8001::153", "2606:50c0:8002::153", "2606:50c0:8003::153"] # GitHub IPs
   ttl     = 3600
 }
 
