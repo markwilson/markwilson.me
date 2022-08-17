fe57bc79280b3ff4c83f3b7e994c96009e27e4d9
Mark Wilson <markwilson@users.noreply.github.com>
2022-08-17 13:17:06 +0100

    Add TXT record for GitHub Pages verification

--- main.tf
+++ main.tf
@@ -87,6 +87,13 @@ resource "aws_route53_record" "site" {
   }
 }
 
+resource "aws_route53_record" "verification" {
+  zone_id = "Z2NO2HNDAEBJY7"
+  name    = "_github-pages-challenge-markwilson.markwilson.me"
+  type    = "TXT"
+  records = ["c5d30a00ecd790beb0f73122665f53"]
+}
+
 data "aws_iam_policy_document" "AWSLambdaTrustPolicy" {
   statement {
     actions = ["sts:AssumeRole"]
